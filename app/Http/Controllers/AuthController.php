82b4d1bb7bce19a6a9995952a2b5bfcf04f49237
cristian <cchura.cpc@gmail.com>
2023-07-21 20:38:17 -0400

    Auth Api Rest Laravel Sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function funIngresar(Request $request){
+        $credenciales = $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        if(!Auth::attempt($credenciales)){
+            return response()->json(["message" => "No Autenticado"]);
+        }
+        
+        $usuario = Auth::user();
+
+        $token = $usuario->createToken("token personal")->plainTextToken;
+
+        return response()->json(["access_token" => $token, "user" => $usuario]);
+
+    }
+
+    public function funRegistro(Request $request){
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required"
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        return response()->json(["message" => "Usuario Registrado"]);
+
+    }
+
+    public function funPerfil(){
+        $usuario = Auth::user();
+
+        return response()->json($usuario);
+    }
+    
+    public function funSalir(){
+        Auth::user()->tokens()->delete();
+
+        return response()->json(["message" => "LogOut"]);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,24 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix("v1/auth")->group(function(){
+
+    Route::post("login", [AuthController::class, "funIngresar"]);
+    Route::post("register", [AuthController::class, "funRegistro"]);
+
+    Route::middleware('auth:sanctum')->group(function(){
+        
+        Route::get("profile", [AuthController::class, "funPerfil"]);
+        Route::post("logout", [AuthController::class, "funSalir"]);
+    });
+    
+});
+
